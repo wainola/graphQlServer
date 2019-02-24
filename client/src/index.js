f026ac206c6c1d2eccc4dcc7d44b7adb7282af11
Nicolás Riquelme <nrriquelme@gmail.com>
2019-02-24 16:54:53 -0300

    adding apollo to client

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:9001/graphql'
+});
+
+client
+  .query({
+    query: gql`
+      {
+        messages {
+          id
+          text
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
