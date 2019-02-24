adea93a7a8c88082473de25cf47864dd750241f2
Nicolás Riquelme <nrriquelme@gmail.com>
2019-02-24 15:01:19 -0300

    WIP resolving issues on seeder

--- seeders/index.js
+++ seeders/index.js
@@ -15,18 +15,58 @@ const queryUsers = `INSERT INTO users (username) VALUES ($1) RETURNING *;`;
 const queryMessages = `INSERT INTO message (text, user_id) VALUES ($1, 
 $2) RETURNING *;`;
 
-Promise.all(
-  users.map(async user => {
-    const userToInsert = [user.username];
-    const q = await conn.query(queryUsers, userToInsert);
-    const result = await q.rows[0];
-    console.log('SUCCESS ON INSERTING USERS', result);
-    const { id: user_id } = result;
-    for (let i = 0; i < messages.length; i++) {
-      const dataToInsert = [messages[i].text, user_id];
-      conn.query(queryMessages, dataToInsert).then(data => {
-        console.log('SUCCESS ON INSERTING MESSAGE', data);
-      });
-    }
-  })
-);
+// const cleanAllTables = () => {
+//   const q1 =
+// }
+
+const resolveUsers = data =>
+  Promise.all(
+    data.map(async item => {
+      try {
+        const valuesToInsert = [item.username];
+        const q = await conn.query(queryUsers, valuesToInsert);
+        const results = await q.rows[0];
+        console.log('Succes on inserting users', results);
+      } catch (e) {
+        console.log('Some error happende on inserting users', e);
+      }
+    })
+  );
+
+const resolveMessages = data =>
+  Promise.all(
+    data.map(async (item, idx) => {
+      const s = `SELECT * FROM USERS;`;
+      try {
+        const q = await conn.query(s);
+        const results = await q.rows;
+
+        const u1 = results.filter(item => item.username === 'wainola')[0];
+        const u2 = results.filter(item => item.username === 'nrriquel')[0];
+        const u3 = results.filter(item => item.username === 'queen')[0];
+        // console.log('USERS:::', u1, u2, u3);
+
+        if (idx < 2) {
+          item.user_id = u1.id;
+        }
+        if (idx >= 2 && idx < 4) {
+          item.user_id = u2.id;
+        }
+        if (idx >= 4 && idx < 6) {
+          item.user_id = u3.id;
+        }
+
+        const valuesToInsert = [item.text, item.user_id];
+        const q2 = await conn.query(queryMessages, valuesToInsert);
+        const r = await q2.rows[0];
+        console.log('SUCCESS ON INSERTING MESSAGE', r);
+      } catch (e) {
+        console.log('Some error happened during message insertion', e);
+      }
+    })
+  );
+
+resolveUsers(users)
+  .then(() => resolveMessages(messages))
+  .then(() => process.exit())
+  .catch(e => console.log('some error happened', e));
