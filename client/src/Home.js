f8697fffabe4593123d87ee045ef6415a71691f2
Nicolás Riquelme <nrriquelme@gmail.com>
2019-02-24 22:25:15 -0300

    Getting data from the server

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,36 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const GET_USERS = gql`
+  {
+    users {
+      id
+      username
+    }
+  }
+`;
 
 export default props => {
-  console.log(props);
-  return <h1>Home</h1>;
+  return (
+    <Query query={GET_USERS}>
+      {({ loading, error, data }) => {
+        if (loading) return 'Loading';
+        if (error) return 'Error';
+        console.log('data', data);
+        return (
+          <div>
+            <ul>
+              {data.users.map(item => (
+                <li>
+                  <p>{item.id}</p>
+                  <p>{item.username}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      }}
+    </Query>
+  );
 };
